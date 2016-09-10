e6bfa452984ae02312396ca7f463002df6635f4c
praneethpagidi <praneethpagidi90@gmail.com>
2016-09-10 19:19:42 -0400

    Pictures on a Map App

---
+++ app/src/androidTest/java/com/praneeth2/picturelocater/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.praneeth2.picturelocater;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
